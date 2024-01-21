414d2c838c2539ceadaac67904c495e7075804c7
latsa <latsapnev01@mail.ru>
2024-01-21 11:48:44 +0300

    add new java class "Book.java"

---
+++ src/Book.java
@@ -0,0 +1,28 @@
+public class Book {
+    private String bookName;
+    private Author author;
+    private int publicationYear;
+
+    public Book(String bookName, Author author, int publicationYear) {
+        this.bookName = bookName;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+    public Author getAuthor() {
+        return author;
+    }
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public String toString() {
+        return "Название книги - " + this.bookName + ", Автор - " + this.author + ", Год публикации - " + this.publicationYear;
+    }
+}
